0f9523bfa05c6920ca0848cf89b45af29da4204d
safiri <safiri@163.com>
2020-06-18 10:14:39 +0800

    update

--- SwiftProjectLibrary-Demo/SwiftProjectLibrary-Demo/AppDelegate.swift
+++ SwiftProjectLibrary-Demo/SwiftProjectLibrary-Demo/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
 //        window?.rootViewController = BaseNavigationController(rootViewController: ViewController())
 //        window?.makeKeyAndVisible()
+        
+        networkListening()
         return true
     }
 
@@ -23,3 +25,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+func networkListening() {
+    AlamofireReachability.reachability.startListening { (status) in
+        switch status {
+        case .notReachable:
+            print("无网络") // WiFi关闭+4G关闭、飞行模式、蜂窝权限关闭
+        case .unknown:
+            print("未知网络")
+        case .reachable(let type):
+            if type == .ethernetOrWiFi {
+                print("ethernetOrWiFi")
+            }else if type == .wwan {
+                print("wwan") // 4G的时候打印了
+            }
+        }
+    }
+}
